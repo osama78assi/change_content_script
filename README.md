5943028f8617186eeb86e70aebcc10afc8301a21
Osama_Assi <138003027+osama78assi@users.noreply.github.com>
2025-06-12 02:46:10 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,177 @@
+# Long Story Short
+What I hate and everyone hate is refactoring it's annoying and costs a lot of time. While there are many tools and ways in text editors to help you change something like quick fixs or even match by Regex. There is small things that theses **Can't** do. And maybe you have a lot of files with the same things to change
+
+# Simple Script
+This may not be the perfect script. and it **shouldn't** be. but it was **very helpful** with be so maybe someone can use it.
+
+This simple script can read all files in passed directory (recursivly if you want) and replace what ever you want by whatever you want also
+
+in my simple case. I have a code in nodeJs with *CommonJs modules* and it has something like this
+
+```js
+const { Request, Response } = require("express");
+
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ */
+async function controller(req, res, next) {
+    try {
+        
+    } catch(err) {
+        next(err);
+    }
+}
+
+module.export = controller;
+```
+
+to replace that there is a quick fix but unfortunately it can't help you. But why ?
+
+it will make it
+
+```js
+import { Request, Response } from "express";
+
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ */
+async function controller(req, res, next) {
+    try {
+        
+    } catch(err) {
+        next(err);
+    }
+}
+
+export default controller;
+```
+
+But that throws an **error** with me
+
+```
+import { Request, Response } from "express";
+         ^^^^^^^
+SyntaxError: Named export 'Request' not found. The requested module 'express' is a CommonJS module, which may n
+CommonJS modules can always be imported via the default export, for example using:
+
+import pkg from 'express';
+const { Request, Response } = pkg;
+
+```
+
+and simpply I don't want to load entire **express** module just for types !
+
+so I will go with simpel solution **CHANGE IT.** But that will take time. A lot of time and I have a lot of files. that annoying simply.
+
+So I said what if I have something that read these files and change what I want ?
+
+let's get over with. I added the script file in **my root directory**
+and wrote this in it. Run it and call it a day
+
+```py
+
+def repl(match: Match[str]):
+    # Change the Request type
+    if match.group(0).startswith("@param {Req"):
+        return '@param {import(\'express\').Request} req'
+    # Change the Response type
+    if match.group(0).startswith("@param {Res"):
+        return '@param {import(\'express\').Response} res'
+    # Remove the require
+    if match.group(0).startswith("const { Request"):
+        return ""
+    # Export default
+    if match.group(0).startswith("module"):
+        return 'export default' + ' ' + match.group(0).split('=')[1]
+    # For safety if we matched something by mistake
+    return match.group(0)
+
+
+change_text_from_files(
+    rec_read_all_files_paths(ignore=['node_modules'], wanted_exts='js'),
+    r'(@param\s*\{\w+\d*\}\s*\w+\d*|const\s*\{\s*Request,\s*Response\s*\}\s*=\s*require\("express"\);\n|module\.export\s*=\s*\w+\d*)',
+    repl
+)
+
+
+```
+so the result become
+
+```js
+
+
+/**
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+async function controller(req, res, next) {
+    try {
+        
+    } catch(err) {
+        next(err);
+    }
+}
+
+export default controller;
+```
+
+### Simple Use Cases
+
+1. When you want some directory not is the root you can pass it with extension. while the default is current working directory
+
+```py
+rec_read_all_files_paths(dir_path="testingMyScript", wanted_exts='js')
+```
+
+or extensions
+
+```py
+rec_read_all_files_paths(wanted_exts=['js', 'ts'])
+```
+
+2. In case you want to exclude some files/folders like node_modules
+
+```py
+rec_read_all_files_paths(ignore=['node_modules', 'keepItSafe.js', 'dontTouchIt.ts'], wanted_exts=['js', 'ts'])
+```
+
+3. If (for some reason) you want to touch hidden files. this script by default doesn't touch them like ignoring .git folder. but you can enable it
+
+```py
+rec_read_all_files_paths(wanted_exts='py', skip_hidden=True)
+```
+
+4. When you want just to get paths from a directory
+
+```py
+# For passing array of extentions
+paths = read_files_paths_from_dir(wanted_exts=['js', 'txt'])
+```
+
+5. In case you want to see all the paths in all directories
+
+```py
+# For reading files in all directories
+for path in rec_read_all_file_paths(ignore=['node_modules'], wanted_exts=['js', 'txt']):
+    print(path)
+```
+
+6. When you want just to do it for a file
+
+```py
+# Change in one file. one thing
+change_text_from_file(
+    r'F:\my_files\myDir\test.js',
+    r'(@param \{Request\} req)',
+    '@param {import(\'express\').Request} req'
+)
+```
+
+btw If you want to make updates or improvements to the code in this repo, feel free to make your changes and open a pull request.
+
+Also, if you have any ideas or suggestions, you're welcome to implement them and submit a PR!
---
+++ main.py
@@ -0,0 +1,186 @@
+from os import listdir, getcwd
+from os.path import isfile, join, isdir
+from typing import List, Callable
+from re import sub, Match, Pattern
+
+
+def is_accepted_extnsion(wanted_exts: str | List[str], extention: str) -> bool:
+    """
+    ### _summary_
+        Check if the passed extension is accepted according to the passed extensions
+    ### Args:
+        wanted_exts (str | List[str]): List of extensions or single extension
+        extention (str): The wanted extension
+
+    ### Returns:
+        bool: Wether the passed extension is accepted or not
+    """
+    if wanted_exts == '*':
+        return True
+    
+    if type(wanted_exts) == str:
+        return extention == wanted_exts
+    
+    if type(wanted_exts) == list:
+        return extention in wanted_exts
+    
+    return False
+
+
+def read_files_paths_from_dir(dir_path=getcwd(), wanted_exts: str | List[str]='*', ignore: list = [], skip_hidden: bool=True) -> list:
+    """
+    ### _summary_
+        This function return all file paths as a list for giving directory path
+    ### Args:
+        dir_path (str, optional): Path to the directory. Defaults to getcwd().
+        ignore (list, optional): Files names to ignore. Defaults to [].
+        wanted_exts (str|list, optional): The wanted extension of files. Defaults to '*'.
+        skip_hidden (bool): Wether to skip hidden files or not. Defaults to True
+
+    ### Raises:
+        ValueError: When the passed path isn't a directory
+        ValueError: When the passed extensions list isn't made of strings
+
+    ### Returns:
+        list: a list of paths
+    """
+    
+    if not isdir(dir_path):
+        raise ValueError('dir_path should be a path to directory not a file')
+    
+    if type(wanted_exts) == list:
+        for ext in wanted_exts:
+            if type(ext) != str:
+                raise ValueError("Extensions list must be made of strings")
+    
+    only_files = []
+    
+    for file_name in listdir(dir_path):
+        if \
+        isfile(join(dir_path, file_name)) \
+        and (not file_name.startswith('.') if skip_hidden else True) \
+        and is_accepted_extnsion(wanted_exts, file_name.split('.')[1]) \
+        and file_name not in ignore:
+            only_files.append(join(dir_path, file_name))
+    
+    return only_files
+
+
+def rec_read_all_files_paths(dir_path=getcwd(), ignore=[], wanted_exts='*', skip_hidden: bool=True) -> list:
+    """
+    ### _summary_
+        Recursively read all files in nested folders and return all paths for the wanted extensions
+    ### Args:
+        dir_path (str, optional): Path to the directory. Defaults to getcwd().
+        ignore (list, optional): Folders or/and Files names to ignore. Defaults to [].
+        wanted_ext (str, optional): The wanted extension of files. Defaults to '*'.
+        skip_hidden (bool): Wether to skip hidden files or not. Defaults to True
+
+    ### Raises:
+        ValueError: When the passed path isn't a directory
+        ValueError: When the passed extensions list isn't made of strings
+
+    ### Returns:
+        list: a list of paths
+    """
+    
+    if not isdir(dir_path):
+        raise ValueError('dir_path should be a path to directory not a file')
+    
+    if type(wanted_exts) == list:
+        for ext in wanted_exts:
+            if type(ext) != str:
+                raise ValueError("Extensions list must be made of strings")
+    all_files = []
+    
+    for child_dir_name in listdir(dir_path):
+        if isdir(join(dir_path, child_dir_name)) \
+            and (not child_dir_name.startswith('.') if skip_hidden else True) \
+            and (child_dir_name not in ignore):
+            all_files.extend(rec_read_all_files_paths(join(dir_path, child_dir_name), ignore, wanted_exts))
+        
+        # Add all files
+        if \
+        isfile(join(dir_path, child_dir_name)) \
+        and (not child_dir_name.startswith('.') if skip_hidden else True) \
+        and is_accepted_extnsion(wanted_exts, child_dir_name.split('.')[1]) \
+        and child_dir_name not in ignore:
+            all_files.append(join(dir_path, child_dir_name))
+    
+    return all_files
+
+
+def change_text_from_file(file_path: str, pattern: str | Pattern[str], repl: str | Callable[[Match[str]], str]):
+    """
+    ### _summary_
+        Take a file path and replace whatever you want with whatever you want too
+
+        Keep in mind if something faild while reading the file it will raise an error.
+    ### Args:
+        file_path (str): The path for the file you want
+        pattern (str | Pattern[str]): The pattern you want to replace
+        repl (str | Callable[[Match[str]], str]): What you want to replace by. If it's a function it will take a Match[str] Object and must return a string
+
+    ### Returns:
+        bool: _description_
+    """
+    content = ""
+    with open(file_path, 'r') as file:
+        content = sub(pattern, repl, file.read())
+    
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+
+def change_text_from_files(files_paths: List[str], pattern: str | Pattern[str], repl: str | Callable[[Match[str]], str]):
+    """
+    ### _summary_
+        Take files paths and replace whatever you want with whatever you want too
+
+        Keep in mind if something faild while reading the file it will raise an error.
+    ### Args:
+        file_path (str): The path for the file you want
+        pattern (str | Pattern[str]): The pattern you want to replace
+        repl (str | Callable[[Match[str]], str]): What you want to replace by. If it's a function it will take a Match[str] Object and must return a string
+
+    ### Returns:
+        bool: _description_
+    """
+    for file_path in files_paths:
+        change_text_from_file(file_path, pattern, repl)
+
+
+if __name__ == "__main__":
+    # Take simple input from terminal But calling the methods as you want and pass replace function is much better and helpful
+    # exts = []
+    # exts_count = int(input("Enter how many extensions you want to include: "))
+    
+    # for _ in range(exts_count):
+    #     exts.append(input("Enter the extension you want: "))
+    
+    # ignore = []
+    # ignore_count = int(input("Enter how many file/folder you want to ignore: "))
+    
+    # for _ in range(ignore_count):
+    #     ignore.append(input("Enter the file/folder name you want to ignore: "))
+    
+    # skip_hidden = False
+    # while not skip_hidden:
+    #     ans = input("Do you want to skip hidden folders/files (y/n): ")
+    #     if ans.lower() == 'n':
+    #         skip_hidden = False
+    #         break
+    #     elif ans.lower() == 'y':
+    #         skip_hidden = True
+    
+    # pattern = input("Enter the pattern you want to match (Regex accpeted): ")
+    
+    # replac_with = input("Enter the value you want to replace with: ")
+    
+    # entry_point = input("Enter your entry point directory: ")
+    
+    # paths = rec_read_all_files_paths(entry_point, ignore, exts)
+    # change_text_from_files(paths, pattern, replac_with)
+    
+    # print("Check now.")
+    pass
